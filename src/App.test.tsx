d9f3d434b664cc2e6b689bf3b1a4dd90d19b0187
Mick F <mick@mickf.net>
2020-11-23 15:19:22 +0100

    test: fix tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Statium will be back soon./i);
   expect(linkElement).toBeInTheDocument();
 });
