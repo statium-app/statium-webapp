7b7a81997e10c905b0c253a63c2348222058994c
Mick F <mick@statium.io>
2020-12-16 00:10:31 +0100

    feat: convert to tailwind CSS

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,34 +1,12 @@
-import React from 'react';
-import styled from 'styled-components';
-
-const CustomHeader = styled.header`
-  border-bottom: solid 1px #ccc;
-`;
-
-const CustomNav = styled.nav`
-  display: flex;
-  margin: 0 auto;
-  padding: 0 1rem;
-  max-width: min(100ch, 100%);
-`;
-
-const HeaderLink = styled.a`
-  padding: 0.5rem;
-  line-height: 1.75rem;
-  display: inline-block;
-  font-size: 1rem;
-  font-weight: bold;
-  text-decoration: none;
-  color: inherit;
-`;
-
 const Header = (): JSX.Element => {
   return (
-    <CustomHeader>
-      <CustomNav>
-        <HeaderLink href="/">Statium</HeaderLink>
-      </CustomNav>
-    </CustomHeader>
+    <header className="border-b border-gray-300">
+      <nav className="container flex mx-auto my-0 py-0 px-4">
+        <a className="inline-block p-2 font-bold leading-7" href="/">
+          Statium
+        </a>
+      </nav>
+    </header>
   );
 };
 
--- src/components/DayCalendar.tsx
+++ src/components/DayCalendar.tsx
@@ -4,7 +4,7 @@ import { FormattedDate, FormattedTime } from 'react-intl';
 import { client } from 'utils/football-data-api-client';
 import { AsyncLoadingState, useAsync } from 'utils/hooks';
 
-import { MainContent, StyledTable, StyledTableBody, StyledTableHeader } from './_styled';
+import { StyledTable, StyledTableBody, StyledTableHeader } from './_styled';
 import { Games } from './types';
 
 const DayCalendar: React.FunctionComponent = () => {
@@ -15,81 +15,98 @@ const DayCalendar: React.FunctionComponent = () => {
     run(client('matches'));
   }, [run]);
 
-  return (
-    <MainContent>
-      {isLoading ? <div>Loading...</div> : null}
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
-      {isError ? (
-        <div>
-          <p>There was an error:</p>
-          <pre>{error?.message}</pre>
-        </div>
-      ) : null}
+  if (isError) {
+    return (
+      <div>
+        <p>There was an error:</p>
+        <pre>{error?.message}</pre>
+      </div>
+    );
+  }
 
-      {isSuccess && data !== null ? (
-        <>
-          <h1>
-            <FormattedDate
-              day="numeric"
-              month="long"
-              value={data?.filters.dateFrom}
-              year="numeric"
-            />
-          </h1>
-          <StyledTable>
-            <StyledTableHeader>
-              <tr>
-                <th>Competition</th>
-                <th>Time</th>
-                <th>Home Team</th>
-                <th>Away Team</th>
-                <th>Status</th>
-                <th>Score</th>
-              </tr>
-            </StyledTableHeader>
-            <StyledTableBody>
-              {data.matches
-                .filter((match: { competition: { area: { code: string }; name: string } }) => {
-                  if (match.competition.area.code === 'BRA') {
-                    return false;
-                  }
+  if (isSuccess && data !== null) {
+    return (
+      <>
+        <header className="bg-gray-800 text-white shadow">
+          <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold leading-tight">
+              <FormattedDate
+                day="numeric"
+                month="long"
+                value={data?.filters.dateFrom}
+                year="numeric"
+              />
+            </h1>
+          </div>
+        </header>
+        <main>
+          <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+            <div className="px-4 py-6 sm:px-0">
+              <div className="border-4 border-dashed border-gray-200 rounded-lg">
+                <StyledTable>
+                  <StyledTableHeader>
+                    <tr>
+                      <th>Competition</th>
+                      <th>Time</th>
+                      <th>Home Team</th>
+                      <th>Away Team</th>
+                      <th>Status</th>
+                      <th>Score</th>
+                    </tr>
+                  </StyledTableHeader>
+                  <StyledTableBody>
+                    {data.matches
+                      .filter(
+                        (match: { competition: { area: { code: string }; name: string } }) => {
+                          if (match.competition.area.code === 'BRA') {
+                            return false;
+                          }
 
-                  if (match.competition.area.code === 'PRT') {
-                    return false;
-                  }
+                          if (match.competition.area.code === 'PRT') {
+                            return false;
+                          }
 
-                  if (
-                    match.competition.area.code === 'ENG' &&
-                    match.competition.name === 'Championship'
-                  ) {
-                    return false;
-                  }
+                          if (
+                            match.competition.area.code === 'ENG' &&
+                            match.competition.name === 'Championship'
+                          ) {
+                            return false;
+                          }
 
-                  return true;
-                })
-                .map((match) => (
-                  <tr>
-                    <td>
-                      {match.competition.name} ({match.competition.area.code})
-                    </td>
-                    <td>
-                      <FormattedTime value={match.utcDate} />
-                    </td>
-                    <td>{match.homeTeam.name}</td>
-                    <td>{match.awayTeam.name}</td>
-                    <td>{match.status}</td>
-                    <td>
-                      {match.score.fullTime.homeTeam} - {match.score.fullTime.awayTeam}
-                    </td>
-                  </tr>
-                ))}
-            </StyledTableBody>
-          </StyledTable>
-          <pre></pre>
-        </>
-      ) : null}
-    </MainContent>
-  );
+                          return true;
+                        }
+                      )
+                      .map((match) => (
+                        <tr>
+                          <td>
+                            {match.competition.name} ({match.competition.area.code})
+                          </td>
+                          <td>
+                            <FormattedTime value={match.utcDate} />
+                          </td>
+                          <td>{match.homeTeam.name}</td>
+                          <td>{match.awayTeam.name}</td>
+                          <td>{match.status}</td>
+                          <td>
+                            {match.score.fullTime.homeTeam} - {match.score.fullTime.awayTeam}
+                          </td>
+                        </tr>
+                      ))}
+                  </StyledTableBody>
+                </StyledTable>
+              </div>
+            </div>
+          </div>
+        </main>
+      </>
+    );
+  }
+
+  return null;
 };
 
 export { DayCalendar };
